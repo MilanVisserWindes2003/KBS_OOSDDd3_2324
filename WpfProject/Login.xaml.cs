b844d583cba177518329d8f4fa266f139082bc24
Leroy <116569478+LeroyRodermond@users.noreply.github.com>
2023-11-16 11:06:09 +0100

    Login Scherm

---
+++ WpfProject/Login.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfProject
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void login_click(object sender, RoutedEventArgs e)
+        {
+            string username = logintxt.Text;
+            string password = wachtwoordtxt.Password;
+        }
+
+        private void registreer_click(object sender, RoutedEventArgs e)
+        {
+            string username = logintxt.Text;
+            string password = wachtwoordtxt.Password;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
