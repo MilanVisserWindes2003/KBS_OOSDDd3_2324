f050ae45cc216cf86103f70e6a110609144c1ef0
MilanVisserWindes2003 <148205046+MilanVisserWindes2003@users.noreply.github.com>
2023-11-23 12:13:37 +0100

    added speech synth

--- WpfProject/SpeachExcersise.xaml.cs
+++ WpfProject/SpeachExcersise.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Speech.Synthesis;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
