35f70534283a3a7efd8cd05f6e89703bd0f246d3
Kiran <kiran.veenendaal.phone@gmail.com>
2023-12-05 14:42:03 +0100

    Login screen revamped + menu screen added. Not all buttons yet working

--- Skepta.Presentation/ViewModel/MenuScreenViewModel.cs
+++ Skepta.Presentation/ViewModel/MenuScreenViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Skepta.Business;
+﻿using System.Windows.Input;
+using Skepta.Business;
+using Skepta.Presentation.ViewModel.Commands;
 
 namespace Skepta.Presentation.ViewModel
 {
@@ -6,9 +8,21 @@ namespace Skepta.Presentation.ViewModel
     {
         private SkeptaModel model;
 
+        public ICommand Excersize => new BaseCommand(ExcersizeCmd);
+        public ICommand Logout => new BaseCommand(LogoutCmd);
         public MenuScreenViewModel(SkeptaModel model)
         {
             this.model = model;
+        }
+
+        public void ExcersizeCmd()
+        {
+            RequestPage = PageId.Exercise;
+        }
+
+        public void LogoutCmd()
+        {
+            RequestPage = PageId.Login;
         }
     }
 }
