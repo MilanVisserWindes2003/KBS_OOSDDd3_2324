28bd1e4aaf43dbbf786d2a324f3d9ba9a89d2908
Kiran <kiran.veenendaal.phone@gmail.com>
2023-12-22 12:11:12 +0100

    Added and removed tests as wel as allowing a method to be public for testsing purpouses

--- SkeptaTests/UnitTests.cs
+++ SkeptaTests/UnitTests.cs
@@ -1,5 +1,6 @@
 using System.Runtime.InteropServices;
 using Skepta.Business;
+using Skepta.Business.ResultsLogic;
 
 namespace SkeptaTests
 {
@@ -43,59 +44,29 @@ namespace SkeptaTests
             Assert.IsFalse(login);
         }
 
-        [TestCase("kiranK", "1234", "1234")]
-        [TestCase("sa", "12", "12" )]
-        [TestCase("BobDeBouwster", "1234", "1234")]
-        [TestCase("LeroyRodmond", "ICT1379!", "ICT1379!")]
+       
+    }
+    [TestFixture]
+    public class ResultsLogicTests
+    {
+        [SetUp]
+        public void Setup() { }
+
+
+        [TestCase("Ik ben Anna. Ik hou van bloemen.", 7)]
+        [TestCase("Mijn naam is Peter. Ik ben 25 jaar oud. Ik werk in een winkel.", 14)]
+        [TestCase("De opkomst van automatisering en kunstmatige intelligentie heeft de aard van werk ingrijpend veranderd. Terwijl technologische vooruitgang nieuwe mogelijkheden creëert, brengt het ook uitdagingen met zich mee, zoals werkloosheid en heropleiding van werknemers. Het vormgeven van de toekomst van werk vereist een holistische benadering, waarbij technologische innovatie wordt gecombineerd met beleidsmaatregelen die sociale rechtvaardigheid en inclusiviteit bevorderen.", 57)]
         [Test]
-        public void CheckRegister_Fine_RetrunsTrue(string username, string password, string password2)
+        public void WordCounting_returnsInt(string randomText, int expectedWords)
         {
             //Arrange
-            var model = new SkeptaModel();
+            var rsl = new ResultsLogic();
 
             //Act
-            var (registerBool, registerString) = model.CheckRegister(username, password, password2);
+            var CountedWords = rsl.WordCounting(randomText);
 
             //Assert
-            Assert.Multiple(() =>
-                {
-                    Assert.IsTrue(registerBool);
-                    Assert.That(registerString.Length, Is.GreaterThan(10));
-                });
-        }
-
-        [TestCase("kiranKkiranKkiranKkiranK", "1234", "1234")]
-        [TestCase("", "12", "12")]
-        [TestCase("BobDeBouwer", "1234", "1234")]
-        [TestCase("LeroyRodmond", "ICT1379!", "")]
-        [TestCase("LeroyRodmond", "", "ICT1379!")]
-        [Test]
-        public void CheckRegister_notFine_RetrunsFalse(string username, string password, string password2)
-        {
-            //Arrange
-            var model = new SkeptaModel();
-
-            //Act
-            var (registerBool, registerString) = model.CheckRegister(username, password, password2);
-
-            //Assert
-            Assert.Multiple(() =>
-            {
-                Assert.IsFalse(registerBool);
-                Assert.That(registerString.Length, Is.GreaterThan(10));
-            });
-        }
-        [Test]
-        public void ObtainRandomText_Text_ReturnTextFromDatabase()
-        {
-            //Arrange 
-            var model = new SkeptaModel();
-
-            //Act 
-            var randomText = model.ObtainRandomText();
-
-            //Assert
-            Assert.That(randomText.Length, Is.GreaterThan(15));
+            Assert.AreEqual(expectedWords, CountedWords);
         }
     }
 }
