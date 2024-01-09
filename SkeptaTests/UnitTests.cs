1dadd2dc503a22014b57fae2e59d284a399ff5c8
Kiran <kiran.veenendaal.phone@gmail.com>
2024-01-09 15:29:20 +0100

    UnitTests updated to reflect current database data

--- SkeptaTests/UnitTests.cs
+++ SkeptaTests/UnitTests.cs
@@ -12,12 +12,12 @@ namespace SkeptaTests
         {
         }
 
-        [TestCase("kiran", "123")]
+        [TestCase("kiran", "1234")]
         [TestCase("s", "1")]
         [TestCase("BobDeBouwer", "1234")]
         [TestCase("LeroyRodermond", "ICT1379!")]
         [Test]
-        public void CheckLogin_NotRight_RetrunsTrue(string username, string password)
+        public void CheckLogin_NotRight_ReturnsTrue(string username, string password)
         {
             //Arrange
             var model = new SkeptaModel();
@@ -28,12 +28,12 @@ namespace SkeptaTests
             Assert.IsTrue(login);
         }
 
-        [TestCase("kiran", "1234")]
+        [TestCase("kiran", "123")]
         [TestCase("s", "12")]
         [TestCase("BobDeBouwer", "123411")]
         [TestCase("LeroyRodermond", "ICT1379!2123")]
         [Test]
-        public void CheckLogin_NotRight_RetrunsFalse(string username, string password)
+        public void CheckLogin_NotRight_ReturnsFalse(string username, string password)
         {
             //Arrange
             var model = new SkeptaModel();
